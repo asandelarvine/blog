e0f93a475938a2ccad0d87bb52d7a7cd55f27ac5
asandelarvine <larvine.asande@student.moringaschool.com>
2021-11-15 10:57:43 +0300

    pushing to heroku

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
   SECRET_KEY = "1234"
-  SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://access:yooh@localhost/blog"
+  SQLALCHEMY_DATABASE_URI = 'postgresql://yglntfnbuxylas:ac70f8c58fe2a0c44d65582347759781e73c407d8b560170ccdc204f5af8cafd@ec2-52-201-195-11.compute-1.amazonaws.com:5432/d7km0aikrrd2ja'
   UPLOADED_PHOTOS_DEST = "app/static/photos"
 
   # email configurations
@@ -14,14 +14,14 @@ class Config:
 
 class ProdConfig(Config):
     
-  SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+  SQLALCHEMY_DATABASE_URI = 'postgresql://yglntfnbuxylas:ac70f8c58fe2a0c44d65582347759781e73c407d8b560170ccdc204f5af8cafd@ec2-52-201-195-11.compute-1.amazonaws.com:5432/d7km0aikrrd2ja'
 
 class TestConfig(Config):
     
-  SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://access:yooh@localhost/blog_test"
+  SQLALCHEMY_DATABASE_URI = 'postgresql://yglntfnbuxylas:ac70f8c58fe2a0c44d65582347759781e73c407d8b560170ccdc204f5af8cafd@ec2-52-201-195-11.compute-1.amazonaws.com:5432/d7km0aikrrd2ja'
 
 class DevConfig(Config):
-  SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://access:yooh@localhost/blog"
+  SQLALCHEMY_DATABASE_URI = 'postgresql://yglntfnbuxylas:ac70f8c58fe2a0c44d65582347759781e73c407d8b560170ccdc204f5af8cafd@ec2-52-201-195-11.compute-1.amazonaws.com:5432/d7km0aikrrd2ja'
   DEBUG = True
 
 config_options = {
